cdbca50ac084833cb8e58a41635e006412cde88e
Adeena-Imran <adeenaimran01@gmail.com>
2025-12-07 23:27:11 +0500

    third commit

--- my-textbook-site/docusaurus.config.ts
+++ my-textbook-site/docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Adeena-Imran.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Humanoid_Robotics_hackathon/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
